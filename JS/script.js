55f9de938efabcea3bdd66a77fc9d5a49b879972
Ashley Whitley <paintrevelations@gmail.com>
2015-03-26 17:04:12 -0700

    Page Setup
    Name and problems

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+/**
+ * Created by MissyGotGame on 3/26/15.
+ */
+/* Ashley Whitley
+Functions Worksheet
+March 26, 2015
+SDI Section 01
+ */
+
+/*Circumference:
+
+    Calculate the circumference of a circle.
+
+Parameter(s) for function:
+Radius of the circle
+Return:
+Circumference of the circle
+Result to print to the console:
+“The circumference of the circle is X”;
+    */
+
+
+
+
+
+/*Stung!
+
+    It takes 8.666666667 bee stings per pound to kill an animal.
+    Calculate how many bee stings are needed to kill an animal in a function
+
+Given:
+Victim’s weight (in pounds)
+Parameter(s) for function:
+Victim’s weight (in pounds)
+Return:
+Number of Bee stings
+Result to print to the console:
+“It takes X bee stings to kill this animal.
+    /*
+
+
+
